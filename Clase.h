c8a55112aeb74acc89be01bcd29a65cff3fe65ee
MariaRosarioMenacho <159058695+MariaRosarioMenacho@users.noreply.github.com>
2024-03-31 00:58:59 -0400

    Clase.h

---
+++ Clase.h
@@ -0,0 +1,15 @@
+Clase Humano {
+private: 
+    int edad;
+    string nombre;
+    string ocupación;
+public:
+    Humano();
+    ~Humano();
+    void set_edad( int e);
+    int get_edad();
+    void set_nombre(string n);
+    string get_nombre();
+    void set_ocupacion(string o);
+     string get_ocupacion();
+};
